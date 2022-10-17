9657ee78a27713ddee906303ab88efcfce92ae7d
Jesse Wagner <jwag1234@hotmail.com>
2022-10-17 13:35:22 -0300

    Trying to fix genre list return

--- Book_Rec_Tool.py
+++ Book_Rec_Tool.py
@@ -5,7 +5,7 @@ import datetime
 
 
 def try_parsing_date(text):
-    for fmt in ("%m/%d/%y", "%Y-%m-%d", "%d.%m.%Y", "%d/%m/%Y", "%Y", ""):
+    for fmt in ("%m/%d/%y", "%Y-%m-%d", "%d.%m.%Y", "%d/%m/%y", "%Y", ""):
         try:
             return datetime.datetime.strptime(text, fmt)
         except ValueError:
@@ -48,15 +48,12 @@ for n in range(len(rows) - 1):
         genres.append(each)
     books_dict["genres"] = genres
     books_dict["pages"] = rows[n][12]
-    try:
-        books_dict["year"] = try_parsing_date(rows[n][15])
-    except ValueError:
-        books_dict["year"] = try_parsing_date(rows[n][14])
-    print(books_dict["year"])
+    books_dict["year"] = rows[n][15]
     books_dict["num_ratings"] = rows[n][17]
     books_dict["liked_percent"] = rows[n][19]
     books_list.append(books_dict)
     books_dict = {}
+genre_list = books_list
 
 
 def language_list():
@@ -92,12 +89,29 @@ def language():
             print(rows[n][1])
 
 
+def min_rating(length_list_complete):
+    rating_choice = float(
+        input(
+            "What is the minimum rating for a book that you would read? (decimal between 0.0-5.0): "
+        )
+    )
+    while type(rating_choice) != "float" and (rating_choice > 5 or rating_choice < 0):
+        rating_choice = input("Please enter a decimal between 0.0 and 5.0: ")
+    rating_list = []
+    for book in length_list_complete:
+        if float(book["rating"]) > rating_choice:
+            rating_list.append(book)
+
+    return rating_list
+
+
 def year_search(genre_list):
     year = input(
         "How recent was the book published? (type year of oldest disreable result): "
     )
     while year not in "0123456789":
         year = input("Please type a year: ")
+        pass
 
 
 # print(languages_sorted())
@@ -142,11 +156,7 @@ def genre_search(genre, genre_list=None):
     return genre_search_list
 
 
-genres = genres()
-# print(random.choice(genres))
-
-
-def genre_choice_search(genre_choice, genre_list=None):
+def genre_choice_search(genre_choice, genre_list):
     while genre_choice not in genres:
         if genre_choice == "help":
             print(genres)
@@ -154,16 +164,10 @@ def genre_choice_search(genre_choice, genre_list=None):
             "Please choose a genre or type 'help' for a full list of options: "
         )
     print("\nShowing books in category: {}\n".format(genre_choice))
-    if genre_list == None:
-        print("Genre list is None")
-        genre_list = genre_search(genre_choice)
-        for genre in genre_list:
-            print(genre["book_name"])
-    else:
-        print("Genre list already exists!")
-        genre_list = genre_search(genre_choice, genre_list)
-        for genre in genre_list:
-            print(genre["book_name"])
+
+    genre_list_new = genre_search(genre_choice, genre_list)
+    for genre in genre_list_new:
+        print(genre["book_name"])
 
     genre_search_again = input(
         "Would you like to add another genre to the search? (y/n): "
@@ -172,18 +176,62 @@ def genre_choice_search(genre_choice, genre_list=None):
         genre_search_again = input("I'm sorry, please choose yes or no. (y/n): ")
 
     if genre_search_again == "y":
-        print(genre_list)
+        print(genre_list_new)
         genre_choice_new = input(
             "What other genre would you list to add? (type help for a full list of genres): "
         )
-        genre_choice_search(genre_choice_new, genre_list)
+
+        genre_choice_search(genre_choice_new, genre_list_new)
+
+    else:
+        return genre_list_new
 
 
-for n in range(len(books_list) - 1):
-    print(books_list[n]["year"])
+def book_length(list_choice):
+    length_list = []
+    length_choice = input(
+        "How long would you like the book to be? \nShort, Medium, Long, or Epic? (s/m/l/e): "
+    )
+    while length_choice not in "smle":
+        length_choice = input(
+            "Sorry, please choose a book length. Short, Medium, Long, or Epic? (s/m/l/e): "
+        )
+    if length_choice == "s":
+        for book in list_choice:
+            print(book)
+            if book["pages"] == "" or int(book["pages"]) < 300:
+                length_list.append(book)
+    elif length_choice == "m":
+        for book in list_choice:
+            if book["pages"] == None or book["pages"] == "":
+                if int(book["pages"]) >= 300 and int(book["pages"]) < 500:
+                    length_list.append(book)
+    elif length_choice == "l":
+        for book in list_choice:
+            if (
+                book["pages"] == ""
+                or int(book["pages"]) >= 500
+                and int(book["pages"]) < 700
+            ):
+                length_list.append(book)
+    elif length_choice == "e":
+        for book in list_choice:
+            if book["pages"] == "" or int(book["pages"]) >= 700:
+                length_list.append(book)
+
+    return length_list
+
+
+genres = genres()
+# print(random.choice(genres))
 
 print("Hello and welcome to BookRec 0.1v!\n")
 genre_choice = input(
     "First off, what genre would you like to search for? (type help for a full list of genres): "
 )
-genre_choice_search(genre_choice)
+genre_list_complete = genre_choice_search(genre_choice, genre_list)
+print(genre_list_complete)
+# length_list_complete = book_length(genre_list_complete)
+# rating_list_complete = min_rating(length_list_complete)
+# for book in rating_list_complete:
+# print(book["book_name"])
